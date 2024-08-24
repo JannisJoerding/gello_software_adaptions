d24f22a9db4389ea86a449cb2aa2b510ec2b9480
JaJoe <jjoerding@techfak.uni-bielefeld.de>
2024-08-24 10:14:11 +0200

    Added correct offsets for left Panda

--- gello/agents/gello_agent.py
+++ gello/agents/gello_agent.py
@@ -64,13 +64,7 @@ PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
     "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT7928LZ-if00-port0": DynamixelRobotConfig(
         joint_ids=(1, 2, 3, 4, 5, 6, 7),
         joint_offsets=(
-            2*np.pi/2,
-            2*np.pi/2,
-            2*np.pi/2,
-            1*np.pi/2,
-            1*np.pi/2,
-            2*np.pi/2,
-            0*np.pi/2
+            2*np.pi/2, 2*np.pi/2, 3*np.pi/2, 1*np.pi/2, 1*np.pi/2, 3*np.pi/2, 0*np.pi/2
         ),
         #joint_signs=(1, -1, 1, 1, 1, -1, 1), #Originale Version
         joint_signs=(1, 1, 1, -1, 1, -1, 1), #Meine Version
